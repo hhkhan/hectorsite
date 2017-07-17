2aa4e8f0f1fa3f05308f7f6e3ecdf1b25e4d854c
hhkhan <hhkilqz@outlook.com>
2017-07-18 00:02:46 +0800

    add jiaodaschool/views.py

--- jiaodaschool/views.py
+++ jiaodaschool/views.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hello, this page is jiaodaschool index page.")
