f8316c1ce4a690dde2f638012d38db2604a4d51d
hhkhan <hhkilqz@outlook.com>
2017-07-23 17:31:59 +0800

    finish write the examine detail view. and try to upload to sinacloud

--- jiaodaschool/urls.py
+++ jiaodaschool/urls.py
@@ -1,6 +1,12 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'jiaodaschool'
+
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^exam/$', views.ExmIndexView.as_view(), name='exm_index'),
+    url(r'^exam/detail/(?P<exm_id>[0-9]+)/$', views.exm_detail, name='exm_detail'),
+    url(r'^students/$', views.index, name='index')
 ]
--- jiaodaschool/views.py
+++ jiaodaschool/views.py
@@ -1,7 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from django.views import generic
+from .models import Examine, Student, Score
+
+
+class ExmIndexView(generic.ListView):
+    model = Examine
+    template_name = 'jiaodaschool/exm_index.html'
+    context_object_name = 'all_exam_list'
+
+def exm_detail(request, exm_id):
+    examine = get_object_or_404(Examine, pk=exm_id)
+    score_set_sorted = sorted(examine.score_set.all(), key=lambda x:x.score, reverse=True)
+    dct_stu_order = {}
+    idx = 0
+    last_score = 800.00
+    last_idx = 1
+    for score in score_set_sorted:
+        idx += 1
+        if abs(float(score.score) - last_score) < 0.001:
+            dct_stu_order[score] = last_idx
+            last_score = float(score.score)
+        else:
+            dct_stu_order[score] = idx
+            last_idx = idx
+            last_score = float(score.score)
+
+    context = {'examine': examine, 'dct_stu_order': dct_stu_order}
+    return render(request, 'jiaodaschool/exm_detail.html', context)
+
 
 # Create your views here.
 def index(request):
